568d418317fb7b53b6a24d970be761782c867fab
HarleZe <96379798+HarleZe@users.noreply.github.com>
2025-01-21 17:12:25 +0100

    Ajout TWIG/CSS

--- index.php
+++ index.php
@@ -1,34 +1,44 @@
 <?php
 
+require_once 'vendor/autoload.php';
 
-// Router Php
+//config twig
+$loader = new \Twig\Loader\FilesystemLoader('./src/views');
+$twig = new \Twig\Environment($loader);
 
+$path = $_SERVER['REQUEST_URI'];
 
-class Router
-{
-
-    private $routes = array(
-        '/' => 'homepage',
-        '/about' => 'about',
-    );
-
-    public function __construct()
-    {
-        $this->route();
-    }
-
-    public function route()
-    {
-        require_once('./src/views/header/header.php');
-        $path = $_SERVER['REQUEST_URI'];
-        if (array_key_exists($path, $this->routes)) {
-            $controller = $this->routes[$path];
+if ($path === '/') {
    
-            require_once('./src/controllers/' . $controller . '.php');
-        } else {
-            require_once('./src/controllers/404.php');
-        }
-    }
-}
+    //simule 5objets pour les derniers objts
+    $lastItems = [
+        ['id' => 101, 'name' => 'Canapé', 'price' => 1500, 'date_added' => '2025-01-21'],
+        ['id' => 102, 'name' => 'Lampe fleurie', 'price' => 800, 'date_added' => '2025-01-20'],
+        ['id' => 103, 'name' => 'Table basse', 'price' => 1200, 'date_added' => '2025-01-19'],
+        ['id' => 104, 'name' => 'Armoire en bois', 'price' => 2500, 'date_added' => '2025-01-18'],
+        ['id' => 105, 'name' => 'Tapis rayé', 'price' => 900, 'date_added' => '2025-01-17'],
+    ];
 
-$router = new Router();
+    echo $twig->render('homepage.twig', [
+        'title' => 'Accueil',
+        'lastItems' => $lastItems
+    ]);
+} elseif ($path === '/items') {
+    //exemple objets fictifs
+    $items = [
+        ['id' => 1, 'name' => 'Table en bois', 'average_price' => 1200],
+        ['id' => 2, 'name' => 'Lampe fleurie', 'average_price' => 800],
+    ];
+    echo $twig->render('items/list.twig', ['items' => $items]);
+} elseif (preg_match('/\/items\/(\d+)/', $path, $matches)) {
+    $itemId = $matches[1];
+    //exemple données fictives
+    $item = ['id' => $itemId, 'name' => 'Table en bois', 'description' => 'Une table élégante en bois.'];
+    $sellers = [
+        ['name' => 'Marie', 'price' => 1100, 'contact' => 'marie@example.com'],
+        ['name' => 'Tom', 'price' => 1250, 'contact' => null],
+    ];
+    echo $twig->render('items/detail.twig', ['item' => $item, 'sellers' => $sellers]);
+} else {
+    echo $twig->render('404.twig', ['title' => 'Page introuvable']);
+}
