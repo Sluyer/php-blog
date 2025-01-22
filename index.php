a17cc970c38c19cf6012218060d8608713b93357
corentinglm <corentin.glm@icloud.com>
2025-01-22 09:30:40 +0100

    Enhance homepage layout and update item details; add new order entry

--- index.php
+++ index.php
@@ -9,20 +9,7 @@ $twig = new \Twig\Environment($loader);
 $path = $_SERVER['REQUEST_URI'];
 
 if ($path === '/') {
-   
-    //simule 5objets pour les derniers objts
-    $lastItems = [
-        ['id' => 101, 'name' => 'Canapé', 'price' => 1500, 'date_added' => '2025-01-21'],
-        ['id' => 102, 'name' => 'Lampe fleurie', 'price' => 800, 'date_added' => '2025-01-20'],
-        ['id' => 103, 'name' => 'Table basse', 'price' => 1200, 'date_added' => '2025-01-19'],
-        ['id' => 104, 'name' => 'Armoire en bois', 'price' => 2500, 'date_added' => '2025-01-18'],
-        ['id' => 105, 'name' => 'Tapis rayé', 'price' => 900, 'date_added' => '2025-01-17'],
-    ];
-
-    echo $twig->render('homepage.twig', [
-        'title' => 'Accueil',
-        'lastItems' => $lastItems
-    ]);
+    require_once('./src/controllers/homepage.php');
 } elseif ($path === '/items') {
     //exemple objets fictifs
     $items = [
--- src/controllers/homepage.php
+++ src/controllers/homepage.php
@@ -4,19 +4,9 @@
 $title = 'Blogue 🐘';
 
 
-// require Articles class
+// Get items
 require_once('./src/models/items.php');
-$itemsModel = new Items();
-$items = $itemsModel->getOne(1337);
-var_dump($items);
+$items = new Items();
+$allItems = $items->getAll();
 
-
-// place an order
-require_once('./src/models/orders.php');
-$ordersModel = new Orders();
-$ordersModel->placeOrder(1337, 42);
-
-require_once('./src/views/navbar.php');
-require_once('./src/views/hero.php');
-require_once('./src/views/homepage/featured.php');
-require_once('./src/views/homepage/homepage.php');
+echo $twig->render('homepage.twig', ['items' => $allItems, 'sellers' => $sellers]);
