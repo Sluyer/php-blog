93bd1ef7844b6f4e8bf9ea1725d8aeec22440647
corentinglm <corentin.glm@icloud.com>
2025-01-22 09:44:00 +0100

    Refactor homepage rendering and enhance layout; remove sellers data and improve image responsiveness

--- src/controllers/homepage.php
+++ src/controllers/homepage.php
@@ -9,4 +9,4 @@ require_once('./src/models/items.php');
 $items = new Items();
 $allItems = $items->getAll();
 
-echo $twig->render('homepage.twig', ['items' => $allItems, 'sellers' => $sellers]);
+echo $twig->render('homepage.twig', ['items' => $allItems]);
